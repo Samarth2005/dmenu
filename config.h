ccc361c8830046e77a7b4f94bb45624d03a4d40d
Samarth <samarthkulshresth@gmail.com>
2020-07-31 07:44:49 +0530

    Minor Changes.

--- config.h
+++ config.h
@@ -6,15 +6,15 @@ static int centered = 1;                    /* -c option; centers dmenu on scree
 static int min_width = 800;                    /* minimum width when centered */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"Mononoki:size=14"
+	"Mononoki:size=12"
 };
 static const char *prompt      = "run: " ;      /* -p option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#e5e9f0", "#2e3440" },
-	[SchemeSel] = { "#eceff4", "#88c0d0" },
-	[SchemeSelHighlight] = { "#d8dee9", "#434c5e" },
-	[SchemeNormHighlight] = { "#8fbcbb", "#4c566a" },
+	[SchemeNorm] = { "#e5e9f0", "#292d3e" },
+	[SchemeSel] = { "#292d3e", "#e1acff" },
+	[SchemeSelHighlight] = { "#d8dee9", "#292d3e" },
+	[SchemeNormHighlight] = { "#8fbcbb", "#292d3e" },
 	[SchemeOut] = { "#3b4252", "#88c0d0" },
 	[SchemeMid] = { "#d8dee9", "#2e3440" },
 };
@@ -29,4 +29,4 @@ static unsigned int lineheight = 22;         /* -h option; minimum height of a m
 static const char worddelimiters[] = " ";
 
 /* Size of the window border */
-static unsigned int border_width = 2;  /* -bw option; to add border width */
+static unsigned int border_width = 0;  /* -bw option; to add border width */
